4e4d1598668296f1c06e522273bdd14fce45524d
aihua <liuaihuajm@126.com>
2016-07-05 09:07:54 +0800

    Initial Commit

---
+++ AHWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AHWeather
+//
+//  Created by aaaa on 16/7/5.
+//  Copyright © 2016年 AiHua Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AHWeather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AHWeather
+//
+//  Created by aaaa on 16/7/5.
+//  Copyright © 2016年 AiHua Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
